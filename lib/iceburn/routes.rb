019511c9a20a61f1bf5be0c1c68f0b21c353070a
Tom Scott <tscott@telvue.com>
2014-05-31 13:35:06 -0400

    Update docs for routes

--- lib/iceburn/routes.rb
+++ lib/iceburn/routes.rb
@@ -1,10 +1,12 @@
 module Iceburn
+  # Extend your `config/routes.rb` file with these to ensure that the
+  # root path is defined and all HTML requests are proxied to Iceburn.
   module Routes
     def iceburn_html!
       get '/*path' => 'application#index'
-      put '/*path' => 'application#index'
-      post '/*path' => 'application#index'
-      delete '/*path' => 'application#index'
+      #put '/*path' => 'application#index'
+      #post '/*path' => 'application#index'
+      #delete '/*path' => 'application#index'
       root to: 'application#index'
     end
   end
