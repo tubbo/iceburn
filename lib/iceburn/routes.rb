56cb91aa98b45225ff210373087ca3e3b51b9600
Tom Scott <tscott@telvue.com>
2014-09-28 20:52:43 -0400

    Regress

--- lib/iceburn/routes.rb
+++ lib/iceburn/routes.rb
@@ -2,9 +2,7 @@ module Iceburn
   # Extend your `config/routes.rb` file with these to ensure that the
   # root path is defined and all HTML requests are proxied to Iceburn.
   module Routes
-    extend ActiveSupport::Concern
-
-    included do
+    def iceburn_html!
       get '/*path' => 'application#index'
       root to: 'application#index'
     end
